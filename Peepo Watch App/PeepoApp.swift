f8c78d18c4dab5c32016dcc20c3d48c16363087a
datalocaltmp <luke@s11research.com>
2026-01-21 18:55:13 -0500

    Initial Commit

---
+++ Peepo Watch App/PeepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeepoApp.swift
+//  Peepo Watch App
+//
+//  Created by Luke on 2026-01-21.
+//
+
+import SwiftUI
+
+@main
+struct Peepo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
